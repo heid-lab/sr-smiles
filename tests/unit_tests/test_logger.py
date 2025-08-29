a45deab7f16823eef1958c417c46a78bf440f8e5
Charlotte Gerhaher <charlotte.gerhaher@tuwien.ac.at>
2025-08-29 13:36:50 +0000

    add logger

--- tests/unit_tests/test_logger.py
+++ tests/unit_tests/test_logger.py
@@ -1,18 +1,6 @@
 import pytest
 
-from cgr_smiles.logger import logger, set_verbose
-
-
-@pytest.fixture
-def propagated_logger():
-    """A fixture to temporarily enable propagation on the logger for testing."""
-    # fixture to enable propagation for the test
-    original_propagation = logger.propagate
-    logger.propagate = True
-    yield logger
-
-    # restore the original setting after testing
-    logger.propagate = original_propagation
+from cgr_smiles.logger import set_verbose
 
 
 @pytest.fixture(autouse=True)
